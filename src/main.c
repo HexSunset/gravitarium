c923374faa511ec8c296cf6821503145d6f7fe13
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-05-29 00:56:04 +0300

    Vector demo

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+#include "vector.h"
+
 int main() {
-  puts("Howdy partner!");
+  Vector2 a = vector2(1, 1);
+  Vector2 b = vector2(-1, 1);
+  printf("a = [%f; %f] with length %f\n", a.x, a.y, vector2_len(a));
+  printf("b = [%f; %f] with length %f\n", b.x, b.y, vector2_len(b));
+  printf("a dot b = %f\n", vector2_dot(a, b));
 
   return 0;
 }
