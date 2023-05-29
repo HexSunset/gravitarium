abf1080b977f9a8d51d61b9c96fac0032da0965a
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-05-30 00:46:17 +0300

    Add TODOs

--- src/main.c
+++ src/main.c
@@ -5,5 +5,10 @@
 #include "simulation.h"
 
 int main() {
+  // TODO: visualize with like raylib or smth.
+  // TODO: load particles from file
+  // TODO: calculate the total applied force to a particle and apply the acceleration based on size of timestep
+  
   return 0;
 }
+
