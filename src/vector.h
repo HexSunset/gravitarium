7f7afb6abb77e41079d3ecfb3ed7d2b897098421
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-05-29 23:47:09 +0300

    make vector.h a header-only thing

--- src/vector.h
+++ src/vector.h
@@ -1,28 +1,61 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
+#include <math.h>
+
 // -- Types --
 typedef struct {
-  double x;
-  double y;
+  double_t x;
+  double_t y;
 } Vector2;
 
 // -- Vector result functions --
 // Create new vector
-Vector2 vector2(double x, double y);
+Vector2 vector2(double_t x, double_t y) {
+  Vector2 v = {x, y};
+  return v;
+}
+
 // Add a and b
-Vector2 vector2_add(Vector2 a, Vector2 b);
+Vector2 vector2_add(Vector2 a, Vector2 b) {
+  Vector2 c = {a.x + b.x, a.y + b.y};
+  return c;
+}
+
 // Subtract b from a
-Vector2 vector2_sub(Vector2 a, Vector2 b);
+Vector2 vector2_sub(Vector2 a, Vector2 b) {
+  Vector2 c = {a.x - b.x, a.y - b.y};
+  return c;
+}
+
 // Multiply v by a scalar
-Vector2 vector2_mul(Vector2 v, double k);
+Vector2 vector2_mul(Vector2 v, double_t k) {
+  Vector2 v2 = {v.x * k, v.y * k};
+  return v2;
+}
+
 // Divide v by a scalar
-Vector2 vector2_div(Vector2 v, double k);
+Vector2 vector2_div(Vector2 v, double_t k) {
+  Vector2 v2 = {v.x / k, v.y / k};
+  return v2;
+}
+
+// Normalize v
+Vector2 vector2_norm(Vector2 v) {
+  double_t l = vector2_len(v);
+  Vector2 v2 = vector2_div(v, l);
+  return v2;
+}
 
 // -- Scalar result functions --
 // Calculate dot product of a and b
-double vector2_dot(Vector2 a, Vector2 b);
+double_t vector2_dot(Vector2 a, Vector2 b) {
+  return a.x * b.x + a.y * b.y;
+}
+
 // Calculate length of v
-double vector2_len(Vector2 v);
+double_t vector2_len(Vector2 v) {
+  return sqrt(v.x * v.x + v.y * v.y);
+}
 
 #endif
